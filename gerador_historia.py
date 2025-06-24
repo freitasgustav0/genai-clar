ae08075f19625f92fb0cb089c550973a438d4bae
freitasgustav0 <54730893+freitasgustav0@users.noreply.github.com>
2025-06-24 14:00:31 -0300

    Update teste gerador_historia.py

--- gerador_historia.py
+++ gerador_historia.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from google import genai
 
-st.set_page_config(page_title="Gerador de Histórias JIRA", layout="wide")
+st.set_page_config(page_title="Gerador de Histórias JIRA ------", layout="wide")
 st.title("Gerador de Histórias JIRA")
 
 st.markdown("""
